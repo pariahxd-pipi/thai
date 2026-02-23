1835391d15b3b6b983f167e4e22f2e7b26431898
pariahxd-pipi <trrjgcynp5@gmail.com>
2026-02-23 15:15:27 +0800

    Update ws.js

--- ws.js
+++ ws.js
@@ -1,10 +1,23 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("travel-app").then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html"
-      ]);
+const CACHE_NAME = "travel-app-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
