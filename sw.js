6500116304b0aa2072cf49484ea52b7c7fd1c5e0
pariahxd-pipi <trrjgcynp5@gmail.com>
2026-02-23 15:27:52 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,19 @@
-const CACHE_NAME = "travel-app-v2";
+const CACHE_NAME = "travel-app-v3";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png",
+  "https://cdn.tailwindcss.com",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/manifest.json",
-        "https://cdn.tailwindcss.com"
-      ]);
+      return cache.addAll(urlsToCache);
     })
   );
 });
@@ -16,12 +21,7 @@ self.addEventListener("install", event => {
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      return response || fetch(event.request).then(fetchRes => {
-        return caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, fetchRes.clone());
-          return fetchRes;
-        });
-      });
+      return response || fetch(event.request);
     })
   );
 });
